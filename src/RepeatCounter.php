8ff346f105736ab2ae8778cb9025d3ee5721f89e
Casey Heitz <heitz.casey@gmail.com>
2015-08-14 11:13:15 -0700

    Add initial web sitefiles and folders

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,18 +4,18 @@
     {
         function countRepeats($input_word, $input_string)
         {
-                $input_word = strtolower($input_word);
-                $input_string = preg_replace("/[^a-zA-Z 0-9]+/", "", $input_string);
-                $input_array = explode(" ", $input_string);
-                $count = 0;
-                foreach($input_array as $word)
-                {
-                    $word = strtolower($word);
-                    if($input_word == $word) {
-                        ++$count;
-                    }
+            $input_word = strtolower($input_word);
+            $input_string = preg_replace("/[^a-zA-Z 0-9]+/", "", $input_string);
+            $input_array = explode(" ", $input_string);
+            $count = 0;
+            foreach($input_array as $word)
+            {
+                $word = strtolower($word);
+                if($input_word == $word) {
+                    ++$count;
                 }
-                return $count;
+            }
+            return $count;
         }
 
         function checkInputWord($input_word)
