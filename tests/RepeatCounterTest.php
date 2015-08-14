34f8012337f6d0c6ff9c003c9bf5d1b885ec4933
Casey Heitz <heitz.casey@gmail.com>
2015-08-14 10:25:12 -0700

    Add and pass third and fourth tests for single words

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -23,7 +23,29 @@
             $test_repeatCounter = new RepeatCounter;
             $input_word = "a";
             $input_string = "b";
-            
+
+            $result = $test_repeatCounter->countRepeats($input_word, $input_string);
+
+            $this->assertEquals(0, $result);
+        }
+
+        function test_oneWord_true()
+        {
+            $test_repeatCounter = new RepeatCounter;
+            $input_word = "hello";
+            $input_string = "hello";
+
+            $result = $test_repeatCounter->countRepeats($input_word, $input_string);
+
+            $this->assertEquals(1, $result);
+        }
+
+        function test_oneWord_false()
+        {
+            $test_repeatCounter = new RepeatCounter;
+            $input_word = "taco";
+            $input_string = "bacon";
+
             $result = $test_repeatCounter->countRepeats($input_word, $input_string);
 
             $this->assertEquals(0, $result);
